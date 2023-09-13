a95323bb6702b83af91f8877846f4c909f4c1d70
Luis Gamboa <progama.end@gmail.com>
2023-09-13 17:54:06 -0600

    change 17:54

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:8000/api/tutorials';
+const baseUrl = 'http://localhost:8080/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
